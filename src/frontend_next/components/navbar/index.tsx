eb74bc0f7ac2338de1ab38de48ef199aa7e7f9e0
AbdouBouallaga <babdelka@student.1337.ma>
2023-01-06 01:47:41 +0100

    update front

--- src/frontend_next/components/navbar/index.tsx
+++ src/frontend_next/components/navbar/index.tsx
@@ -5,24 +5,12 @@ import Link from "next/link";
 import Image from "next/image";
 import axios from "axios";
 
-// function logout() {
-//   const fetchData = async () => {
-//     // get the data from the api
-//     const data = await fetch('http://127.0.0.1.nip.io/api/auth/logout');
-
-//     Router.push('/login');
-//     // window.location.href = "http://127.0.0.1.nip.io/login";
-
-//   }
-//   const result = fetchData()
-//   // make sure to catch any error
-//   .catch(console.error);
-// }
 const Navbar = ({ profile } :any) => {
-  const router = useRouter();
-  console.log(profile);
+
+  const [NavDrop, setNavDrop] = useState(false);
   return (
-    <nav className="px-2">
+  <>
+    <nav className="">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
         <Link href="/" className="flex items-center">
           {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-10" alt="Flowbite Logo" /> */}
@@ -30,86 +18,8 @@ const Navbar = ({ profile } :any) => {
             PingPong
           </span>
         </Link>
-        {/* <button data-collapse-toggle="navbar-dropdown" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
-        <span className="sr-only">Open main menu</span>
-        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-      </button> */}
         <div className="hidden w-full md:block md:w-auto">
           <ul className="flex  p-4  items-center  flex-row space-x-8  text-sm font-medium  ">
-            <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent"
-                aria-current="page"
-              >
-                profile
-              </a>
-            </li>
-            <li>
-              {/* <button
-                id="dropdownNavbarLink"
-                data-dropdown-toggle="dropdownNavbar"
-                className="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-gray-400 dark:hover:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
-              >
-                Dropdown{" "}
-                <svg
-                  className="w-5 h-5 ml-1"
-                  aria-hidden="true"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </button> */}
-              {/* <!-- Dropdown menu --> */}
-              <div
-                id="dropdownNavbar"
-                className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
-              >
-                <ul
-                  className="py-1 text-sm text-gray-700 dark:text-gray-400"
-                  aria-labelledby="dropdownLargeButton"
-                >
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Dashboard
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Settings
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Earnings
-                    </a>
-                  </li>
-                </ul>
-                <div className="py-1">
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
-                  >
-                    Sign out
-                  </a>
-                </div>
-              </div>
-            </li>
             <li>
               <a
                 href="#"
@@ -135,6 +45,9 @@ const Navbar = ({ profile } :any) => {
               </svg>
             </li>
             <li>
+              <a onClick={()=>{
+                setNavDrop(!NavDrop)
+            }}>
               <Image
                 className="w-10 h-10 rounded-full"
                 width={100}
@@ -142,11 +55,57 @@ const Navbar = ({ profile } :any) => {
                 src={profile?.avatar}
                 alt={profile?.username}
               />
+              </a>
             </li>
           </ul>
         </div>
       </div>
+    { NavDrop &&
+    <div
+     id="dropdownNavbar"
+     className="z-100 bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+      >
+     <ul
+       className="py-1 text-sm text-gray-700 dark:text-gray-400"
+       aria-labelledby="dropdownLargeButton"
+     >
+       <li>
+         <a
+           href="#"
+           className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+         >
+           Dashboard
+         </a>
+       </li>
+       <li>
+         <a
+           href="#"
+           className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+         >
+           Settings
+         </a>
+       </li>
+       <li>
+         <a
+           href="#"
+           className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+         >
+           Earnings
+         </a>
+       </li>
+     </ul>
+     <div className="py-1">
+       <a
+         href="#"
+         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
+       >
+         Sign out
+       </a>
+     </div>
+    </div>
+}
     </nav>
+</>
   );
 };
 
