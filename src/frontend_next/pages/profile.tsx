c383d25e5aaffaaf0a87052fa1008fd4bc4c3299
AbdouBouallaga <abdou.bouallaga@live.fr>
2023-01-11 01:43:28 +0100

    Profile edit working

--- src/frontend_next/pages/profile.tsx
+++ src/frontend_next/pages/profile.tsx
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from "react";
 import Router, {useRouter} from 'next/router';
 import axios from "axios";
 import { Modal, Button, TextInput, Badge} from "flowbite-react";
+import ImageResize from 'image-resize';
+import { clear } from "console";
+
+var imageResize = new ImageResize({
+  format: 'png',
+  width: 160
+});
 
 const Profile = () => {
   // State to store the user's profile data
@@ -23,6 +30,8 @@ const Profile = () => {
   const [qr2faConfirm, set2faConfirm] = useState(false);
   const [qr2faCodeError, set2faCodeError] = useState(false);
   const [enabled2fa, set2faEnabled] = useState(false);
+  const [editError, setEditError] = useState(false);
+  const [editReloadContent, setReloadContent] = useState(1);
 
   function modal2faDefault(){
     toggle2faModal();
@@ -32,12 +41,16 @@ const Profile = () => {
     set2faCodeError(false);
   }
 
-  const toggle2faModal = () => setModal(!enable2famodal);
   const [enable2famodal, setModal] = useState(false);
+  const toggle2faModal = () => setModal(!enable2famodal);
+
+  const [enableEditmodal, setEditModal] = useState(false);
+  const toggleEditModal = () => setEditModal(!enableEditmodal);
 
   async function enable2fa() {
     axios.get('api/auth/tfa/generate')
     .then((response) => {
+      console.log(response.data);
       set2fa(response.data);
       toggle2faModal();
       })
@@ -102,6 +115,50 @@ const Profile = () => {
       }
   }
 
+  async function PushEdits(Username:string, imgInput:string) {
+    var imgResized = imageResize.play(imgInput)
+    .then((resizedImage) => {
+      console.log(resizedImage);
+      axios({
+        method: 'POST',
+        url: '/api/users/me',
+        data: {
+          username: Username,
+          avatar: resizedImage
+        },
+      })
+      .then((response) => {
+        if (response.data.login42){
+          setEditModal(false);
+          setEdit(false);
+          toggleEditModal();
+          Router.reload(); // reload l7za9 kaml
+          // setReloadContent(editReloadContent + 1); // this reload the profile but not the navbar
+        }
+      })
+      .catch((error) => {
+        setEditError(true)
+      })
+    }
+    )
+  }
+
+  async function ProcessEdits() {
+    const TextInput = document.getElementById('username') as HTMLInputElement;
+    const FileInput = document.getElementById('avatar') as HTMLInputElement;
+    let maxSize:number = 6145*1024;
+    let inputSize:number = FileInput.files[0].size;
+    console.log(maxSize);
+    console.log(inputSize);
+    var imgInput:string = profile.avatar;
+    if (FileInput.files[0] && maxSize > inputSize){
+      imgInput = FileInput.files[0];
+    }
+    PushEdits(TextInput.value, imgInput);
+    FileInput.value = "";
+
+  }
+
 
   // Fetch the user's profile data when the component mounts
   useEffect(() => {
@@ -123,7 +180,7 @@ const Profile = () => {
         });
     }
     fetchProfile();
-  }, [enabled2fa]);
+  }, [enabled2fa, editReloadContent]);
 
   // Function to toggle edit mode
 
@@ -137,12 +194,40 @@ const Profile = () => {
       <h1><b>{profile.username}</b></h1>
 
       <>
-        {/* <p>Username: {profile.username}</p> */}
-        {/* <p>Score: {profile.score}</p> */}
-        <div className="">
-        <Button className='m-2' onClick={() => setEdit((edit) => !edit)}>Edit</Button>
+        
+        {/* EDIT button and modal*/}
         <React.Fragment>
-        {profile.tfaEnabled ? <Button color="failure" onClick={toggle2faModal} >Disable 2fa</Button> : <Button className='m-2' onClick={enable2fa}>Enable 2fa</Button>}
+          <Button className='m-2' onClick={toggleEditModal}>Edit</Button>
+          <Modal  show={enableEditmodal}
+                  onClose={toggleEditModal}
+                  size="sm"
+          >
+            <Modal.Header>Edit Profile</Modal.Header>
+            <Modal.Body>
+              {editError ?? <Badge color="failure">Error editing profile</Badge>}
+              <div className="form-group">
+                <label>Username</label>
+                <TextInput id="username" className='form-control' type="text" defaultValue={profile.username} />
+              </div>
+              <div className="form-group">
+                <label>Avatar</label>
+                <input type="file" className="form-control-file" id="avatar" max-size="1" accept="image/*" />
+              </div>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={ProcessEdits}>
+                Save
+              </Button>
+              <Button color="failure" onClick={toggleEditModal}>
+                Cancel
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </React.Fragment>
+
+        {/* 2FA button and modal*/}
+        <React.Fragment>
+        {profile.tfaEnabled ? <Button className='m-2' color="failure" onClick={toggle2faModal} >Disable 2fa</Button> : <Button className='m-2' onClick={enable2fa}>Enable 2fa</Button>}
           <Modal  show={enable2famodal}
                   onClose={modal2faDefault}
                   size="sm"
@@ -222,111 +307,10 @@ const Profile = () => {
             </Modal.Footer>
           </Modal>
         </React.Fragment>
-        {/* <Button className='btn btn-primary m-2' onClick={enable2fa}>Enable 2fa</Button> */}
-        </div>
       </>
-      {edit ? <ModalEdit setEdit={setEdit} profile={profile} /> : null}
     </div>
-    
     </>
   );
 };
 
 export default Profile;
-
-const ModalEdit = ({ setEdit, profile }) => {
-
-  const [createObjectURL, setCreateObjectURL] = useState(null);
-  const uploadToClient = (event) => {
-    console.log("in change")
-    if (event.target.files && event.target.files[0]) {
-      const i = event.target.files[0];
-
-      // console.log(i)
-      // setImage(i);
-      setCreateObjectURL(URL.createObjectURL(i));
-    }
-  };
-
-  return (
-    <div className="fixed top-0 left-0 right-0 z-6  w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
-      <div className="relative w-full h-full max-w-md md:h-auto">
-        {/* <!-- Modal content --> */}
-        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          {/* <!-- Modal header --> */}
-          <div className="flex items-center justify-between p-5 border-b rounded-t dark:border-gray-600">
-            <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-              Edit Profile
-            </h3>
-            <Button
-              type="Button"
-              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-              onClick={() => setEdit((edit) => !edit)}
-            >
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              <span className="sr-only">Close modal</span>
-            </Button>
-          </div>
-          {/* <!-- Modal body --> */}
-          <div className="p-6 space-y-6">
-            <div className="relative avatar mt-10  ">
-              <img className="absolute" src={createObjectURL} alt={profile.username} />
-              <TextInput type="file" name="myImage" accept="image/*" className="opacity-0 absolute"  onChange={uploadToClient}/>
-            </div>
-            <div>
-              <label
-                htmlFor="first_name"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                First name
-              </label>
-              <TextInput
-                type="text"
-                id="first_name"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              />
-            </div>
-            <div>
-              <label className="inline-flex relative items-center cursor-pointer">
-                <TextInput type="checkbox" value="" className="sr-only peer" />
-                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-                  Toggle me
-                </span>
-              </label>
-            </div>
-          </div>
-          {/* <!-- Modal footer --> */}
-          <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-            <Button
-              data-modal-toggle="small-modal"
-              type="Button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Edit it
-            </Button>
-            <Button
-              data-modal-toggle="small-modal"
-              type="Button"
-              className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
-            >
-              Decline
-            </Button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
