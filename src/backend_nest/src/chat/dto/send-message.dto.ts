2d2cc579338d362c379ecd2e498ba6a89cb5c149
Ayoub Berdai <aberdai@e2r4p7.1337.ma>
2023-01-28 06:18:48 +0100

    ChatService dev

---
+++ src/backend_nest/src/chat/dto/send-message.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class SendMessageDto {
+	@IsNotEmpty()
+	@IsString()
+	login42: string;
+
+	@IsNotEmpty()
+	@IsNumber()
+	channelId: number;
+
+	@IsNotEmpty()
+	@IsString()
+	content: string;
+}
--- src/backend_nest/src/main.ts
+++ src/backend_nest/src/main.ts
@@ -3,8 +3,6 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 
-
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
