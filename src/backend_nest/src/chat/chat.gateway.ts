87e40ee4058bc48389c78e6725775ecd79a0bdad
AbdouBouallaga <babdelka@student.1337.ma>
2023-02-06 02:37:20 +0100

    init frontend for sendMessage event

--- src/backend_nest/src/chat/chat.gateway.ts
+++ src/backend_nest/src/chat/chat.gateway.ts
@@ -1,6 +1,8 @@
 import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
 import { Server, Socket } from 'socket.io';
+import { ChatServerService } from './chat-server.service';
+import { SendMessageDto } from './dto';
 
 @WebSocketGateway({ namespace: 'chat' })
 export class ChatGateway {
@@ -10,5 +12,21 @@ export class ChatGateway {
   socketToChannelMap: Map<string, number>;
   socketToLogin42Map: Map<string, string>;
 
-  constructor(private readonly chatService: ChatService) {}  
+  constructor(private readonly chatService: ChatService,
+              private readonly chatServerService: ChatServerService) {}  
+
+  // @SubscribeMessage('enterRoom')
+  // handleEnterRoom(@MessageBody() data: EnterRoomDto, @ConnectedSocket() client: Socket) : any {
+  //   this.socketToChannelMap.set(client.id, data.channelId);
+  //   this.socketToLogin42Map.set(client.id, data.login42);
+  //   this.server.in(client.id).socketsJoin(`Channel_${data.channelId}`);
+  // }
+  @SubscribeMessage('sendmessage')
+  async handlesendmessage(@MessageBody() data :SendMessageDto, @ConnectedSocket() client: Socket)  {
+    console.log("sendmessage received", data);
+    await this.chatServerService.sendMessage(data);
+    client.emit("updateChatSection", "fg");
+  }
+
+
 }
--- src/frontend_next/components/chat/ChatSection.tsx
+++ src/frontend_next/components/chat/ChatSection.tsx
@@ -11,20 +11,35 @@ import {
   Alert,
 } from "flowbite-react";
 import { Router, useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import Games from "../../components/icons/Games";
 import Menu from "../../components/icons/Menu";
+import { GeneralContext } from "../../pages/_app";
 import { Search } from "../icons";
 
 import Drawer from "./Drawer";
 import EditRoom from "./EditRoom";
 
+
 const ChatSection = ({ profile }: any) => {
+  const Context: any = useContext(GeneralContext);
+  const socket:any = Context.ChatSocket; 
+  let init = useRef<boolean>(false); 
+  const router = useRouter();
   const [messages, setMessages] = useState([]);
   const [data, setData] = useState({});
-  const router = useRouter();
   const { id } = router.query;
-
+console.log("message",messages )
+  useEffect(()=>{
+    if (!init.current)
+    {
+      init.current = false;
+      socket.on("updateChatSection", (data:any)=>{
+        console.log(data);
+        // setMessages(data)
+      })
+    }
+  }, [])
   useEffect(() => {
     id && fetchChat(id as string);
   }, [id]);
@@ -44,18 +59,17 @@ const ChatSection = ({ profile }: any) => {
   return (
     <div className="grow m-2 flex flex-col ">
       <HeaderOfChat profile={profile} data={data} />
-      <BodyOfChat messages={messages} profile={profile} />
+      <BodyOfChat messages={messages} profile={profile} roomid={id}/>
     </div>
   );
 };
-
-export default ChatSection;
-
-const role = (members: any, username: string) => {
-  const role = ["member", "admin", "owner"];
-  const member = members?.find((member: any) => member.username === username);
-  return member && role[member.role];
-};
+  
+  
+  const role = (members: any, username: string) => {
+    const role = ["member", "admin", "owner"];
+    const member = members?.find((member: any) => member.username === username);
+    return member && role[member.role];
+  };
 
 const HeaderOfChat = ({ profile, data }: any) => {
   const { isDM, members, name } = data;
@@ -64,7 +78,7 @@ const HeaderOfChat = ({ profile, data }: any) => {
   const [drawer, setDrawer] = useState(false);
   const [editRoom, setEditRoom] = useState(false);
   const [invite, setInvite] = useState(false);
-
+  
   return (
     <div className="border-b border-gray-600 flex items-center justify-between mx-3">
       {/* avatar and userName */}
@@ -76,7 +90,7 @@ const HeaderOfChat = ({ profile, data }: any) => {
           rounded={true}
           status="online" // online, offline, busy, away
           alt="user"
-        />
+          />
 
         {isDM ? (
           <span className="text-xs text-slate-300 hidden md:block">
@@ -84,7 +98,7 @@ const HeaderOfChat = ({ profile, data }: any) => {
           </span>
         ) : (
           ""
-        )}
+          )}
         <span className=" text-base"> {name}</span>
       </div>
 
@@ -97,8 +111,8 @@ const HeaderOfChat = ({ profile, data }: any) => {
       {/* setting */}
       {isDM ? (
         ""
-      ) : (
-        <Dropdown
+        ) : (
+          <Dropdown
           label={
             <div className="hover:rounded-full p-1 hover:bg-slate-300">
               <Menu />
@@ -121,7 +135,7 @@ const HeaderOfChat = ({ profile, data }: any) => {
             onClick={() => {
               setDrawer(true);
             }}
-          >
+            >
             All Members
           </Dropdown.Item>
           <Dropdown.Item>Leave</Dropdown.Item>
@@ -133,13 +147,13 @@ const HeaderOfChat = ({ profile, data }: any) => {
         myRole={myRole}
         members={members}
         profile={profile}
-      />
+        />
       <Modal
         show={editRoom}
         size="md"
         popup={true}
         onClose={() => setEditRoom(false)}
-      >
+        >
         <Modal.Header />
         <Modal.Body>
           <EditRoom setEditRoom={setEditRoom} data={data} />
@@ -150,7 +164,7 @@ const HeaderOfChat = ({ profile, data }: any) => {
         size="md"
         popup={true}
         onClose={() => setInvite(false)}
-      >
+        >
         <Modal.Header>Invite your friends to join chat</Modal.Header>
         <Modal.Body>
           <InviteToRoom />
@@ -179,8 +193,8 @@ const Msg = ({ date, message, username }: any) => {
 const MyMsg = ({ date, message, username }: any) => {
   return (
     <div
-      className="mb-4 max-w-[80%]
-        m-1 flex flex-col items-end self-end"
+    className="mb-4 max-w-[80%]
+    m-1 flex flex-col items-end self-end"
     >
       <div className="">
         <span className="text-black font-medium ">{username} &nbsp;</span>
@@ -193,7 +207,7 @@ const MyMsg = ({ date, message, username }: any) => {
   );
 };
 
-const BodyOfChat = ({ messages, profile }: any) => {
+const BodyOfChat = ({ messages, profile, roomid }: any) => {
   const { avatar, username: myUserName } = profile;
   console.log("the message", messages);
   return (
@@ -204,42 +218,54 @@ const BodyOfChat = ({ messages, profile }: any) => {
           if (username === myUserName) {
             return (
               <MyMsg
-                key={i}
-                date={date}
-                message={message}
-                username={username}
+              key={i}
+              date={date}
+              message={message}
+              username={username}
               />
-            );
-          } else {
+              );
+            } else {
             return (
               <Msg key={i} date={date} message={message} username={username} />
             );
           }
         })}
       </div>
-      <SendMsg />
+      <SendMsg roomid={roomid}/>
     </div>
   );
 };
 
-const SendMsg = () => {
+const SendMsg = (data:any) => {
+  const Context: any = useContext(GeneralContext);
+  const profile:any = Context.Profile;
+  const socket:any = Context.ChatSocket; 
+  const [myMessage,setMyMessage]= useState("")
+  const handleSubmit = (e:any)=>{
+    e.preventDefault();
+    socket.emit("sendmessage", {login42: profile.login42, channelId:parseInt(data.roomid) , myMessage});
+    setMyMessage('')
+   
+
+  }
   
   return (
     <div className="w-full mt-4">
-      <form className="w-full" autoComplete="off">
+      <form onSubmit={handleSubmit} className="w-full" autoComplete="off">
         <div className="relative w-full">
           <input
+            value={myMessage}
+            onChange={(e)=>{setMyMessage(e.target.value)}}
             type="search"
             id="default-search"
             className="block w-full p-4  text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Type a message ..."
             required
-          />
+            />
           <button
-            type="button"
+            type="submit"
             className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            onClick={()=>{}}
-          >
+            >
             Send
           </button>
         </div>
@@ -253,7 +279,7 @@ const InviteToRoom = () => {
     e.preventDefault();
     console.log("submit");
   };
-
+  
   return (
     <>
       <form onSubmit={handlSubmit}>
@@ -267,31 +293,31 @@ const InviteToRoom = () => {
             rightIcon={Search}
             placeholder="find user to add"
             // required={true}
-          />
+            />
         </div>
       </form>
       <div className="flex-1 m-2">
         <div className="flex flex-row flex-wrap overflow-auto max-h-[300px]">
           {/* {results.map((e: any, i: number) => (
             <div
-              key={i}
-              className="relative m-2"
-              style={{ width: 80 }}
-              onClick={() => {
-                setSearchModal(!searchModal);
-                setResults([]);
-                if (searchRef.current !== null) searchRef.current.value = "";
-                Router.push(`/profile/` + e?.username);
-              }}
+            key={i}
+            className="relative m-2"
+            style={{ width: 80 }}
+            onClick={() => {
+              setSearchModal(!searchModal);
+              setResults([]);
+              if (searchRef.current !== null) searchRef.current.value = "";
+              Router.push(`/profile/` + e?.username);
+            }}
             >
-              <Avatar
-                alt="Nav Drop settings"
-                img={e?.avatar}
-                rounded={false}
-                size="lg"
-                // status="online"
-              />
-              <div className="font-bold aero w-full">{e.username}</div>
+            <Avatar
+            alt="Nav Drop settings"
+            img={e?.avatar}
+            rounded={false}
+            size="lg"
+            // status="online"
+            />
+            <div className="font-bold aero w-full">{e.username}</div>
             </div>
           ))} */}
         </div>
@@ -299,3 +325,5 @@ const InviteToRoom = () => {
     </>
   );
 };
+
+export default ChatSection;
